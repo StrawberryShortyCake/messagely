24308bd81e8ada3e1f5947e81c8b7ba8b5641c2c
Andrea Jao <ndrjao@gmail.com>
2024-05-21 12:31:40 -0700

    updated routes!

--- routes/users.js
+++ routes/users.js
@@ -11,12 +11,34 @@ const router = new Router();
  *
  **/
 
+router.get("/",
+  async function (req, res) {
+
+    const results = await User.all();
+
+    return res.json({ users: results });
+  }
+);
+
 
 /** GET /:username - get detail of users.
  *
  * => {user: {username, first_name, last_name, phone, join_at, last_login_at}}
  *
  **/
+
+router.get("/:username",
+  async function (req, res) {
+
+    const username = req.params.username;
+    console.log("getting username for /username route", username);
+
+    const results = await User.get(username);
+
+    return res.json({ users: results });
+  }
+);
+
 
 
 /** GET /:username/to - get messages to user
@@ -29,6 +51,18 @@ const router = new Router();
  *
  **/
 
+router.get("/:username/to",
+  async function (req, res) {
+
+    const username = req.params.username;
+    console.log("getting username for /username/to route", username);
+
+    const results = await User.messagesTo(username);
+
+    return res.json({ messages: results });
+  }
+);
+
 
 /** GET /:username/from - get messages from user
  *
@@ -39,5 +73,17 @@ const router = new Router();
  *                 to_user: {username, first_name, last_name, phone}}, ...]}
  *
  **/
+
+router.get("/:username/from",
+  async function (req, res) {
+
+    const username = req.params.username;
+    console.log("getting username for /username/from route", username);
+
+    const results = await User.messagesTo(username);
+
+    return res.json({ messages: results });
+  }
+);
 
 export default router;
